63f793b541c254615f5e2cd0676e765592420ba4
Paolo-dev-it <paolo.nicoletti99@gmail.com>
2022-11-14 20:45:54 +0100

    Added comments. Project and bonuses completed!

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,30 +3,27 @@ var app = new Vue({
   data: {
     toDo: [
       {
-        description: "comprare il pane",
+        description: "Comprare il pane",
         done: false,
       },
       {
-        description: "comprare la pasta",
+        description: "Comprare la pasta",
         done: false,
       },
     ],
-    text: "",
+    text: "", //Variabile che prende l'input della nuova task
   },
 
   methods: {
-
-    toDoTasks(){
-        
-    },
-
-     toDoEliminate( index){     
+    //Funzione che elimina la task quando premo l'icona 'x'
+     toDoEliminate( index ){     
          this.toDo.splice(index, 1)
      },
 
-     toDoDone(index){  
+     //Funzione che mi permette di barrare e sbarrare la task quando l'ho completata
+     toDoDone( index ){  
           
-          if(this.toDo[index].done == true){
+          if( this.toDo[index].done == true ){
             return this.toDo[index].done = false
           } else {
             return this.toDo[index].done = true
@@ -34,16 +31,13 @@ var app = new Vue({
           
      },
 
+     //Funzione che aggiunge la nuova task dall'input all'array di oggetti e la aggiungo alla lista
      addTask(){
         
             this.toDo.push({
                 description: this.text,
                 done: false,
             })
-        
      },
-
-     
   },
-  
 });
