62fa5bc91c4766e39ec6828aecb303d690e04abe
Paolo-dev-it <paolo.nicoletti99@gmail.com>
2022-11-14 14:58:24 +0100

    Created files and folders

---
+++ assets/js/main.js
@@ -0,0 +1,15 @@
+var app = new Vue({
+  el: "#root",
+  data: {
+    toDo: [
+      {
+        description: "comprare il pane",
+      },
+      {
+        description: "comprare la pasta",
+      },
+    ],
+  },
+
+  methods: {},
+});
